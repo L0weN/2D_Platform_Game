56df271266fcbb5f04956f88a826c7e9748ea17b
Mert <mertozata009@gmail.com>
2022-04-02 19:53:04 +0300

    2D_Platform_Game_V1.5
    Enemy bird, spider and frog added.

---
+++ Assets/Scripts/Enemies Scripts/SpiderScripts.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderScripts : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D myBody;
+
+    private Vector3 moveDirection = Vector3.down;
+
+    private string coroutine_Name = "ChangeMovement";
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(coroutine_Name);
+    }
+
+    void Update()
+    {
+        moveSpider();
+    }
+
+    void moveSpider()
+    {
+        transform.Translate(moveDirection * Time.smoothDeltaTime);
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        yield return new WaitForSeconds(Random.Range(2f, 5f));
+        if (moveDirection == Vector3.down)
+        {
+            moveDirection = Vector3.up;
+        }
+        else
+        {
+            moveDirection = Vector3.down;
+        }
+
+        StartCoroutine(coroutine_Name);
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == MyTags.BULLET_TAG)
+        {
+            anim.Play("SpiderDead");
+            myBody.bodyType = RigidbodyType2D.Dynamic;
+            StartCoroutine(SpiderDead());
+            StopCoroutine(coroutine_Name);
+        }
+    }
+
+    IEnumerator SpiderDead()
+    {
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(false);
+    }
+}
